eed8f09ace8bb067f689ea69adb2ab680b184bbf
Daniel Czerwonk <czerwonk@users.noreply.github.com>
2018-11-11 19:13:21 +0100

    Update main.go
    bumped versiion

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ import (
 	"github.com/prometheus/common/log"
 )
 
-const version string = "0.4.3"
+const version string = "0.4.4"
 
 var (
 	showVersion   = flag.Bool("version", false, "Print version information")
